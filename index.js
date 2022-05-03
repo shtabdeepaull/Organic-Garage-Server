f02f873cf688704c7d0d50af5dfbc5c75dd2dd3f
shtabdeepaull <shtabdee30@gmail.com>
2022-05-03 22:22:05 +0600

    connected to database

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+const app =express();
+
+// used middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gfrpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('warehouse DB Connected' )
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Running warehouse server');
+});
+
+
+app.listen(port, () => {
+    console.log('Listening to server')
+})
