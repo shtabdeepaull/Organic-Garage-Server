7cc6f4d3551cf77941b3a099088409e2157bc8b8
shtabdeepaull <shtabdee30@gmail.com>
2022-05-08 02:06:11 +0600

    added pagiantion

--- index.js
+++ index.js
@@ -23,21 +23,30 @@ async function run() {
     const productCollection = client.db('WarehouseManagement').collection('product');
 
     app.get('/product', async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
       const query = {};
       const cursor = productCollection.find(query);
-      const products = await cursor.toArray();
+      let products;
+      if (page || size) {
+        products = await cursor.skip(page*size).toArray();
+      }
+      else {
+        products = await cursor.toArray();
+      }
+
       res.send(products);
     });
 
     app.get('/product/:id', async (req, res) => {
       const id = req.params.id;
-      const query ={_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const product = await productCollection.findOne(query);
       res.send(product);
     });
 
     // POST
-    app.post('/product', async(req, res) => {
+    app.post('/product', async (req, res) => {
       const newItem = req.body;
       const result = await productCollection.insertOne(newItem);
       res.send(result);
@@ -46,8 +55,8 @@ async function run() {
     app.get('/productCount', async (req, res) => {
       const query = {};
       const cursor = productCollection.find(query);
-      const count = await cursor.count();
-      res.send({count});
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count });
     })
   }
   finally {
