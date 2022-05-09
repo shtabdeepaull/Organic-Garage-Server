22854f90cf82b3634b6563920b92ce6924c977eb
shtabdeepaull <shtabdee30@gmail.com>
2022-05-09 16:22:16 +0600

    added delete api

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
 
+
+const port = process.env.PORT || 5000;
 const app = express();
 
 // used middleware
 app.use(cors());
 app.use(express.json());
 
-
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'unauthorized access'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if(err) {
+      return res.status(403).send({message: 'Forbidden Access'});
+    }
+    req.decoded = decoded;
+    next();
+  })
+      
+}
 
 
 
@@ -25,11 +40,11 @@ async function run() {
 
   //AUTH
    app.post('/login', async(req, res) => {
-    const user =req.body;
-    const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN,{
+    const user = req.body;
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN,  {
       expiresIn: '1d'
     });
-    res.send(accessToken);
+    res.send({accessToken});
    })   
 
     // products API 
@@ -49,6 +64,7 @@ async function run() {
       res.send(products);
     });
 
+
     app.get('/product/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
@@ -56,12 +72,18 @@ async function run() {
       res.send(product);
     });
 
-    app.get('/product', async(req, res) => {
+    app.get('/product', verifyJWT, async(req, res) => {
+      const decodedEmail = req.decoded.email; 
       const email = req.query.email;
-      const query={email: email};
+      if(email === decodedEmail) {
+        const query={email: email};
       const cursor = productCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
+      }
+      else{
+        res.status(403).send({message: 'Forbidden Access'})
+      }
     })
 
     // POST
@@ -70,6 +92,15 @@ async function run() {
       const result = await productCollection.insertOne(newItem);
       res.send(result);
     });
+
+    // delete product
+    app.delete('/product/:id', async (req, res) => {
+      const id =req.params.id;
+      const query ={_id: ObjectId(id)};
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+
+    })
 
     // product count API
     app.get('/productCount', async (req, res) => {
